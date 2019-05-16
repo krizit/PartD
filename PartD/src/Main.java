f48e1947eca05dacda215e668a2199c433ec6ed2
krizit <krizit@gmail.com>
2019-05-17 02:59:27 +1200

    fourth commit

--- PartD/src/Main.java
+++ PartD/src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 		System.out.println("Hi, my name is Joel");
 		System.out.println("My favourite colour is blue");
 		System.out.println("My favourite mode of transportation is car");
+		System.out.println("The best day of the week is friday");
 	}
 
 }
